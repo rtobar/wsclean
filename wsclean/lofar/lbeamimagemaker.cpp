879b6d2acea70f914489b956f243b12ee51454a6
André Offringa <offringa@gmail.com>
2016-03-11 15:53:47 +0100

    Fixing issues for compiling without LOFAR lib

---
+++ wsclean/lofar/lbeamimagemaker.cpp
@@ -0,0 +1,347 @@
+#include "lbeamimagemaker.h"
+
+#ifndef HAVE_LOFAR_BEAM
+#include <stdexcept>
+void LBeamImageMaker::Make(std::vector<ImageBufferAllocator::Ptr>&)
+{
+	throw std::runtime_error("LOFAR beam imager maker called, but the software has been compiled without the LOFAR beam. Recompile your software and make sure that cmake finds the LOFAR station response library.");
+}
+#else
+
+#include "../angle.h"
+#include "../banddata.h"
+#include "../fftresampler.h"
+#include "../fitsreader.h"
+#include "../fitswriter.h"
+#include "../imagecoordinates.h"
+#include "../imageweights.h"
+#include "../matrix2x2.h"
+#include "../progressbar.h"
+#include "../uvector.h"
+
+#include "../wsclean/imageweightcache.h"
+#include "../wsclean/logger.h"
+#include "../msproviders/msprovider.h"
+#include "../multibanddata.h"
+
+#include <StationResponse/LofarMetaDataUtil.h>
+
+#include <casacore/ms/MeasurementSets/MSField.h>
+
+#include <casacore/tables/Tables/ArrayColumn.h>
+#include <casacore/tables/Tables/TableRecord.h>
+#include <casacore/tables/Tables/ScalarColumn.h>
+
+#include <casacore/measures/Measures/MDirection.h>
+#include <casacore/measures/Measures/MEpoch.h>
+#include <casacore/measures/Measures/MPosition.h>
+#include <casacore/measures/Measures/MCPosition.h>
+
+#include <casacore/measures/TableMeasures/ScalarMeasColumn.h>
+#include <casacore/measures/TableMeasures/ArrayMeasColumn.h>
+
+#include <stdexcept>
+
+using namespace LOFAR::StationResponse;
+
+static void dirToITRF(const casacore::MDirection& dir, casacore::MDirection::Convert& convert, vector3r_t& itrf);
+
+void LBeamImageMaker::Make(std::vector<ImageBufferAllocator::Ptr>& beamImages)
+{
+	_sampledWidth = _width / _undersample;
+	_sampledHeight = _width / _undersample;
+	_sPixelSizeX = _pixelSizeX * _undersample;
+	_sPixelSizeY = _pixelSizeX * _undersample;
+
+	for(size_t i=0; i!=8; ++i)
+	{
+		for(size_t j=0; j!=_width*_height; ++j)
+			beamImages[i][j] = 0.0;
+	}
+	
+	_totalWeightSum = 0.0;
+	
+	for(size_t i=0; i!=_msProviders.size(); ++i)
+	{
+		makeBeamForMS(beamImages, *_msProviders[i].provider, _tableEntry->msData[i], *_msProviders[i].selection);
+	}
+
+	for(size_t i=0; i!=8; ++i)
+	{
+		for(size_t j=0; j!=_sampledWidth*_sampledHeight; ++j)
+		{
+			beamImages[i][j] /= _totalWeightSum;
+		}
+	}
+	
+	FFTResampler resampler(_sampledWidth, _sampledHeight, _width, _height, 1);
+	ImageBufferAllocator::Ptr scratch;
+	_allocator->Allocate(_width*_height, scratch);
+	for(size_t p=0; p!=8; ++p)
+	{
+		resampler.RunSingle(&beamImages[p][0], scratch.data());
+		memcpy(&beamImages[p][0], scratch.data(), sizeof(double)*_width*_height);
+	}
+}
+
+void LBeamImageMaker::makeBeamForMS(std::vector<ImageBufferAllocator::Ptr>& beamImages, MSProvider& msProvider, const ImagingTableEntry::MSInfo& msInfo, const MSSelection& selection)
+{
+	casacore::MPosition arrayPos;
+	casacore::MDirection delayDir, tileBeamDir;
+	
+	/**
+		* Read some meta data from the measurement set
+		*/
+	casacore::MeasurementSet& ms = msProvider.MS();
+	if(ms.nrow() == 0) throw std::runtime_error("Table has no rows (no data)");
+	
+	casacore::MSAntenna aTable = ms.antenna();
+	if(aTable.nrow() == 0) throw std::runtime_error("No antennae in set");
+	casacore::MPosition::ROScalarColumn antPosColumn(aTable, aTable.columnName(casacore::MSAntennaEnums::POSITION));
+	arrayPos = antPosColumn(0);
+	
+	MultiBandData band(ms.spectralWindow(), ms.dataDescription());
+	double centralFrequency = 0.0;
+	for(size_t b=0; b!=msInfo.bands.size(); ++b)
+	{
+		BandData subBand(band[b], selection.ChannelRangeStart(), selection.ChannelRangeEnd());
+		centralFrequency += subBand.CentreFrequency();
+	}
+	centralFrequency /= msInfo.bands.size();
+	Logger::Debug << "Making beam for frequency " << centralFrequency * 1e-6 << " MHz.\n";
+	
+	casacore::MSField fieldTable(ms.field());
+	casacore::ROScalarMeasColumn<casacore::MDirection> delayDirColumn(fieldTable, casacore::MSField::columnName(casacore::MSFieldEnums::DELAY_DIR));
+	if(fieldTable.nrow() != 1)
+		throw std::runtime_error("Set has multiple fields");
+	delayDir = delayDirColumn(0);
+	
+	if(fieldTable.tableDesc().isColumn("LOFAR_TILE_BEAM_DIR")) {
+		casacore::ROArrayMeasColumn<casacore::MDirection> tileBeamDirColumn(fieldTable, "LOFAR_TILE_BEAM_DIR");
+		tileBeamDir = *(tileBeamDirColumn(0).data());
+	} else {
+		throw std::runtime_error("LOFAR_TILE_BEAM_DIR column not found");
+	}
+	
+	std::vector<Station::Ptr> stations(aTable.nrow());
+	readStations(ms, stations.begin());
+	
+	Logger::Debug << "Counting timesteps...\n";
+	std::vector<size_t> idToMSRow;
+	msProvider.MakeIdToMSRowMapping(idToMSRow, selection);
+	
+	casacore::MEpoch::ROScalarColumn timeColumn(ms, ms.columnName(casacore::MSMainEnums::TIME));
+	casacore::MEpoch time = timeColumn(idToMSRow[0]);
+	casacore::MEpoch endTime = timeColumn(idToMSRow.back());
+	double totalSeconds = endTime.get("s").getValue() - time.get("s").getValue();
+	size_t intervalCount = (totalSeconds + _secondsBeforeBeamUpdate - 1) / _secondsBeforeBeamUpdate;
+	Logger::Debug << "MS spans " << totalSeconds << " seconds, dividing in " << intervalCount << " intervals.\n";
+	std::vector<size_t> timestepIds(1, 0);
+	for(size_t id=0;id!=idToMSRow.size();++id)
+	{
+		if(timeColumn(idToMSRow[id]).getValue() != time.getValue())
+		{
+			timestepIds.push_back(id);
+			time = timeColumn(idToMSRow[id]);
+		}
+	}
+	
+	size_t timestepCount = timestepIds.size();
+	timestepIds.push_back(idToMSRow.size());
+	
+	casacore::MEpoch midTime(casacore::MVEpoch(0.5 * (timeColumn(0).getValue().get() + timeColumn(ms.nrow()-1).getValue().get())), timeColumn(0).getRef());
+	casacore::MeasFrame midFrame(arrayPos, midTime);
+	const casacore::MDirection::Ref hadecRef(casacore::MDirection::HADEC, midFrame);
+	const casacore::MDirection::Ref azelgeoRef(casacore::MDirection::AZELGEO, midFrame);
+	const casacore::MDirection::Ref midJ2000Ref(casacore::MDirection::J2000, midFrame);
+	
+	double refIntervalWeight = 0.0;
+	
+	for(size_t intervalIndex=0; intervalIndex!=intervalCount; ++intervalIndex)
+	{
+		size_t timestepStart = intervalIndex*timestepCount/intervalCount;
+		size_t timestepEnd = (intervalIndex+1)*timestepCount/intervalCount;
+		size_t intervalStartId = timestepIds[timestepStart];
+		size_t intervalEndId = timestepIds[timestepEnd];
+	
+		// Find the mid time step
+		casacore::MEpoch firstTime = timeColumn(idToMSRow[intervalStartId]);
+		casacore::MEpoch lastTime = timeColumn(idToMSRow[intervalEndId-1]);
+		time = casacore::MEpoch(casacore::MVEpoch(0.5 * (firstTime.getValue().get() + lastTime.getValue().get())), firstTime.getRef());
+		
+		casacore::MeasFrame frame(arrayPos, time);
+		const casacore::MDirection::Ref j2000Ref(casacore::MDirection::J2000, frame);
+		
+		Logger::Debug << "Making snapshot beam for timesteps " << timestepStart << " - " << timestepEnd << "\n";
+		
+		double intervalWeight = 0.0;
+		ao::uvector<double> stationWeights(stations.size(), 0.0);
+		WeightMatrix baselineWeights(stations.size());
+		calculateStationWeights(_imageWeightCache->Weights(), intervalWeight, stationWeights, baselineWeights, msProvider, msInfo, selection, idToMSRow, intervalStartId, intervalEndId);
+		
+		if(refIntervalWeight == 0.0)
+			refIntervalWeight = intervalWeight;
+		Logger::Debug << "Relative weight of this interval: " << (intervalWeight * 100.0 / refIntervalWeight) << " %\n";
+		
+		ao::uvector<double> singleImages[8];
+			
+		double *imgPtr[8];
+		for(size_t i=0; i!=8; ++i)
+		{
+			singleImages[i].assign(_sampledWidth*_sampledHeight, 0.0);
+			imgPtr[i] = &singleImages[i][0];
+		}
+		
+		makeBeamSnapshot(stations, stationWeights, baselineWeights, imgPtr, time.getValue().get()*86400.0, centralFrequency, centralFrequency, frame, delayDir, tileBeamDir);
+		
+		_totalWeightSum += intervalWeight;
+		for(size_t i=0; i!=8; ++i)
+		{
+			for(size_t j=0; j!=_sampledWidth*_sampledHeight; ++j)
+			{
+				beamImages[i][j] += singleImages[i][j] * intervalWeight;
+			}
+		}
+	}
+}
+
+static void dirToITRF(const casacore::MDirection& dir, casacore::MDirection::Convert& convert, vector3r_t& itrf)
+{
+	casacore::MDirection itrfDir = convert(dir);
+	casacore::Vector<double> itrfVal = itrfDir.getValue().getValue();
+	itrf[0] = itrfVal[0];
+	itrf[1] = itrfVal[1];
+	itrf[2] = itrfVal[2];
+}
+
+void LBeamImageMaker::makeBeamSnapshot(const std::vector<Station::Ptr>& stations, const ao::uvector<double>& weights, const WeightMatrix& baselineWeights, double** imgPtr, double time, double frequency, double subbandFrequency, const casacore::MeasFrame& frame, const casacore::MDirection& delayDir, const casacore::MDirection& tileBeamDir)
+{
+	static const casacore::Unit radUnit("rad");
+	const casacore::MDirection::Ref itrfRef(casacore::MDirection::ITRF, frame);
+	const casacore::MDirection::Ref j2000Ref(casacore::MDirection::J2000, frame);
+	casacore::MDirection::Convert
+		j2000ToITRFRef(j2000Ref, itrfRef);
+		
+	vector3r_t station0, tile0;
+	dirToITRF(delayDir, j2000ToITRFRef, station0);
+	dirToITRF(tileBeamDir, j2000ToITRFRef, tile0);
+
+	ProgressBar progressBar("Constructing beam");
+	for(size_t y=0;y!=_sampledHeight;++y)
+	{
+		for(size_t x=0;x!=_sampledWidth;++x)
+		{
+			double l, m, ra, dec;
+			ImageCoordinates::XYToLM(x, y, _sPixelSizeX, _sPixelSizeY, _sampledWidth, _sampledHeight, l, m);
+			l += _phaseCentreDL; m += _phaseCentreDM;
+			ImageCoordinates::LMToRaDec(l, m, _phaseCentreRA, _phaseCentreDec, ra, dec);
+			
+			casacore::MDirection imageDir(casacore::MVDirection(
+							casacore::Quantity(ra, radUnit),
+							casacore::Quantity(dec,radUnit)),
+							j2000Ref);
+			
+			vector3r_t itrfDirection;
+			dirToITRF(imageDir, j2000ToITRFRef, itrfDirection);
+			
+			std::vector<MC2x2> stationGains(stations.size());
+			for(size_t a=0; a!=stations.size(); ++a)
+			{
+				Station::Ptr s=stations[a];
+				matrix22c_t gainMatrix = s->response(time, frequency, itrfDirection, subbandFrequency, station0, tile0);
+				stationGains[a][0] = gainMatrix[0][0];
+				stationGains[a][1] = gainMatrix[0][1];
+				stationGains[a][2] = gainMatrix[1][0];
+				stationGains[a][3] = gainMatrix[1][1];
+			}
+			
+			std::complex<double> gain[4] = { 0.0, 0.0, 0.0, 0.0 };
+			double totalWeight = 0.0;
+			for(size_t a=0; a!=stations.size(); ++a)
+			{
+				double w = sqrt(weights[a]);
+				Matrix2x2::MultiplyAdd(gain, stationGains[a].Data(), w);
+				totalWeight += w;
+			}
+			Matrix2x2::ScalarMultiply(gain, 1.0/totalWeight);
+			
+			for(size_t i=0; i!=4; ++i)
+			{
+				*imgPtr[i*2] = gain[i].real();
+				*imgPtr[i*2 + 1] = gain[i].imag();
+				++imgPtr[i*2];
+				++imgPtr[i*2 + 1];
+			}
+		}
+		progressBar.SetProgress(y, _sampledHeight);
+	}
+	progressBar.SetProgress(_sampledHeight, _sampledHeight);
+}
+
+void LBeamImageMaker::calculateStationWeights(const ImageWeights& imageWeights, double& totalWeight, ao::uvector<double>& weights, WeightMatrix& baselineWeights, MSProvider& msProvider, const ImagingTableEntry::MSInfo& msInfo, const MSSelection& selection, const std::vector<size_t>& idToMSRow, size_t intervalStartIdIndex, size_t intervalEndIdIndex)
+{
+	casacore::MeasurementSet& ms = msProvider.MS();
+	casacore::MSAntenna antTable(ms.antenna());
+	totalWeight = 0.0;
+	weights.assign(antTable.nrow(), 0.0);
+	
+	casacore::ROScalarColumn<int> ant1Col(ms, ms.columnName(casacore::MeasurementSet::ANTENNA1));
+	casacore::ROScalarColumn<int> ant2Col(ms, ms.columnName(casacore::MeasurementSet::ANTENNA2));
+	MultiBandData multiband(ms.spectralWindow(), ms.dataDescription());
+	size_t channelCount = selection.ChannelRangeEnd() - selection.ChannelRangeStart();
+	ao::uvector<float> weightArr(channelCount);
+	
+	size_t id = intervalStartIdIndex;
+	while(msProvider.CurrentRowAvailable() && id < intervalEndIdIndex)
+	{
+		int
+			a1 = ant1Col(idToMSRow[id]),
+			a2 = ant2Col(idToMSRow[id]);
+		
+		size_t dataDescId;
+		double uInM, vInM, wInM;
+		msProvider.ReadMeta(uInM, vInM, wInM, dataDescId);
+		const BandData &band(multiband[dataDescId]);
+		msProvider.ReadWeights(weightArr.data());
+		
+		for(size_t ch=0; ch!=channelCount; ++ch)
+		{
+			double
+				u = uInM / band.ChannelWavelength(ch),
+				v = vInM / band.ChannelWavelength(ch);
+			double iw = imageWeights.GetWeight(u, v);
+			if(a1 != a2)
+			{
+				double w = weightArr[ch] * iw;
+				totalWeight += w;
+				weights[a1] += w;
+				weights[a2] += w;
+				baselineWeights.Value(a1, a2) += w;
+			}
+		}
+		msProvider.NextRow();
+		id = msProvider.RowId();
+	}
+	
+	if(Logger::IsVerbose())
+		logWeights(ms, weights);
+}
+
+void LBeamImageMaker::logWeights(casacore::MeasurementSet& ms, const ao::uvector<double>& weights)
+{
+	casacore::MSAntenna antTable(ms.antenna());
+	Logger::Debug << "Weights:";
+	casacore::ROScalarColumn<casacore::String> antNameCol(antTable, casacore::MSAntenna::columnName(casacore::MSAntenna::NAME));
+	double maxWeight = 0.0;
+	for(size_t a=0; a!=antTable.nrow(); ++a)
+		maxWeight=std::max(weights[a], maxWeight);
+	
+	for(size_t a=0; a!=antTable.nrow(); ++a)
+	{
+		std::string name = antNameCol(a);
+		Logger::Debug << ' ' << name << '=' << round(weights[a]*1000.0/maxWeight)*0.1;
+	}
+	Logger::Debug << '\n';
+}
+
+#endif
---
+++ wsclean/wsclean/imagefilename.h
@@ -0,0 +1,116 @@
+#ifndef IMAGE_FILENAME_H
+#define IMAGE_FILENAME_H
+
+#include "wscleansettings.h"
+
+#include "../polarizationenum.h"
+
+class ImageFilename
+{
+public:
+	ImageFilename() :
+		_polarization(Polarization::StokesI),
+		_channelIndex(0),
+		_intervalIndex(0),
+		_isImaginary(false)
+	{
+	}
+	
+	ImageFilename(size_t channelIndex, size_t intervalIndex) :
+		_polarization(Polarization::StokesI),
+		_channelIndex(channelIndex),
+		_intervalIndex(intervalIndex),
+		_isImaginary(false)
+	{
+	}
+	
+	std::string GetPrefix(const WSCleanSettings& settings) const
+	{
+		return GetPrefix(settings, _polarization, _channelIndex, _intervalIndex, _isImaginary);
+	}
+	
+	std::string GetBeamPrefix(const WSCleanSettings& settings) const
+	{
+		return GetBeamPrefix(settings, _polarization, _channelIndex, _intervalIndex, _isImaginary);
+	}
+	
+	void SetPolarization(PolarizationEnum p) { _polarization = p; }
+	void SetIsImaginary(bool isImaginary) { _isImaginary = isImaginary; }
+	
+	static std::string GetPSFPrefix(const WSCleanSettings& settings, size_t channelIndex, size_t intervalIndex)
+	{
+		std::ostringstream partPrefixNameStr;
+		partPrefixNameStr << settings.prefixName;
+		if(settings.intervalsOut != 1)
+			partPrefixNameStr << "-t" << fourDigitStr(intervalIndex);
+		if(settings.channelsOut != 1)
+			partPrefixNameStr << '-' << fourDigitStr(channelIndex);
+		return partPrefixNameStr.str();
+	}
+	
+	static std::string GetPrefix(const WSCleanSettings& settings, PolarizationEnum polarization, size_t channelIndex, size_t intervalIndex, bool isImaginary)
+	{
+		std::ostringstream partPrefixNameStr;
+		partPrefixNameStr << settings.prefixName;
+		if(settings.intervalsOut != 1)
+			partPrefixNameStr << "-t" << fourDigitStr(intervalIndex);
+		if(settings.channelsOut != 1)
+			partPrefixNameStr << '-' << fourDigitStr(channelIndex);
+		if(settings.polarizations.size() != 1)
+		{
+			partPrefixNameStr << '-' << Polarization::TypeToShortString(polarization);
+			if(isImaginary)
+				partPrefixNameStr << 'i';
+		}
+		return partPrefixNameStr.str();
+	}
+	
+	static std::string GetBeamPrefix(const WSCleanSettings& settings, PolarizationEnum polarization, size_t channelIndex, size_t intervalIndex, bool isImaginary)
+	{
+		std::ostringstream partPrefixNameStr;
+		partPrefixNameStr << settings.prefixName;
+		if(settings.intervalsOut != 1)
+			partPrefixNameStr << "-t" << fourDigitStr(intervalIndex);
+		if(settings.channelsOut != 1)
+			partPrefixNameStr << '-' << fourDigitStr(channelIndex);
+		partPrefixNameStr << "-beam-" << Polarization::TypeToShortString(polarization);
+		if(isImaginary)
+			partPrefixNameStr << 'i';
+		return partPrefixNameStr.str();
+	}
+	
+	static std::string GetMFSPrefix(const WSCleanSettings& settings, PolarizationEnum polarization, size_t intervalIndex, bool isImaginary, bool isPSF)
+	{
+		std::ostringstream partPrefixNameStr;
+		partPrefixNameStr << settings.prefixName;
+		if(settings.intervalsOut != 1)
+			partPrefixNameStr << "-t" << fourDigitStr(intervalIndex);
+		if(settings.channelsOut != 1)
+			partPrefixNameStr << "-MFS";
+		if(settings.polarizations.size() != 1 && !isPSF)
+		{
+			partPrefixNameStr << '-' << Polarization::TypeToShortString(polarization);
+			if(isImaginary)
+				partPrefixNameStr << 'i';
+		}
+		return partPrefixNameStr.str();
+	}
+	
+private:
+	PolarizationEnum _polarization;
+	size_t _channelIndex;
+	size_t _intervalIndex;
+	bool _isImaginary;
+	
+	static std::string fourDigitStr(size_t val)
+	{
+		std::ostringstream str;
+		if(val < 1000) str << '0';
+		if(val < 100) str << '0';
+		if(val < 10) str << '0';
+		str << val;
+		return str.str();
+	}
+};
+
+#endif
