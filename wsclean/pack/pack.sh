d62a1debd0a103054fa45b9613ccbe920a8b4ad3
André Offringa <offringa@gmail.com>
2016-05-25 15:58:02 +0200

    Modifying pack script

--- wsclean/pack/pack.sh
+++ wsclean/pack/pack.sh
@@ -19,7 +19,7 @@ else
 		mkdir /tmp/wsclean/wsclean/examples
 		cd ..
 		cp -v CMakeLists.txt Doxyfile.in angle.h application.* areaset.* banddata.* buffered_lane.* casamaskreader.* dftpredictionalgorithm.* fftconvolver.* fftresampler.* fftwmultithreadenabler.* fitsiochecker.* fitsreader.* fitswriter.* gaussianfitter.h imagecoordinates.* imageoperations.* imageweights.* lane.* multibanddata.* nlplfitter.* numberlist.* matrix2x2.* modelrenderer.* msselection.* ndppp.* polarizationenum.* polynomialfitter.* progressbar.* radeccoord.* stopwatch.* system.* threadpool.* uvector.* weightmode.* wscleanmain.cpp wscversion.h /tmp/wsclean/
-		cp -v aocommon/lane.h aocommon/lane_03.h aocommon/lane_11.h aocommon/uvector.h aocommon/uvector_03.h /tmp/wsclean/aocommon/
+		cp -v aocommon/lane.h aocommon/lane_03.h aocommon/lane_11.h aocommon/uvector_11.h aocommon/uvector_03.h /tmp/wsclean/aocommon/
 		cp -v CMake/*.cmake /tmp/wsclean/CMake/
 		cp -v deconvolution/*.{h,cpp} /tmp/wsclean/deconvolution/
 		cp -v idg/*.{h,cpp} /tmp/wsclean/idg/
