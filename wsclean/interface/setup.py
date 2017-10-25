17c9152cb4ad21b7ebaa19b6b3a5c7a43070626d
André Offringa <offringa@gmail.com>
2017-10-25 14:23:26 +0200

    Making python interface compile again

--- wsclean/interface/setup.py
+++ wsclean/interface/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup, Extension
 
 # define the extension module
-_wsclean = Extension('_wsclean', sources=['_wsclean.c'], libraries=['../build/wsclean-shared','casa_ms','casa_tables','casa_casa','casa_measures','casa_fits','fftw3','fftw3_threads','boost_date_time','boost_system','boost_thread','boost_filesystem','cfitsio','gsl'])
+_wsclean = Extension('_wsclean', sources=['_wsclean.c'], libraries=['../build/wsclean-shared','casa_ms','casa_tables','casa_casa','casa_measures','casa_fits','fftw3','fftw3_threads','boost_date_time','boost_system','boost_thread','boost_filesystem','boost_program_options','cfitsio','gsl'])
 
 # run the setup
 setup(ext_modules=[_wsclean])
