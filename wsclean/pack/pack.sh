36facf7d4f9546eec51c3967f183da9a6db587bd
André Offringa <offringa@gmail.com>
2017-12-01 10:20:14 +0100

    Adding aterm files to pack script

--- wsclean/pack/pack.sh
+++ wsclean/pack/pack.sh
@@ -6,6 +6,7 @@ else
 		rm -rf /tmp/wsclean /tmp/wsclean-${VERSION}
 		mkdir /tmp/wsclean
 		mkdir /tmp/wsclean/aocommon
+		mkdir /tmp/wsclean/aterms
 		mkdir /tmp/wsclean/CMake
 		mkdir /tmp/wsclean/deconvolution
 		mkdir /tmp/wsclean/idg
@@ -22,6 +23,7 @@ else
 		cd ..
 		cp -v CMakeLists.txt Doxyfile.in angle.h application.* areaset.* banddata.* buffered_lane.* casamaskreader.* dftpredictionalgorithm.* fftconvolver.* fftresampler.* fftwmultithreadenabler.* fitsiochecker.* fitsreader.* fitswriter.* gaussianfitter.h imagecoordinates.* image.* imageweights.* lane.* multibanddata.* nlplfitter.* numberlist.* matrix2x2.* modelrenderer.* msselection.* ndppp.* polarization.* polynomialchannelfitter.* polynomialfitter.* progressbar.* radeccoord.* rmsimage.* stopwatch.* system.* threadpool.* uvector.* weightmode.* wscleanmain.cpp wscversion.h /tmp/wsclean/
 		cp -v aocommon/lane.h aocommon/lane_03.h aocommon/lane_11.h aocommon/uvector_11.h aocommon/uvector_03.h /tmp/wsclean/aocommon/
+		cp -v aterms/*.{h,cpp} /tmp/wsclean/aterms/
 		cp -v CMake/*.cmake /tmp/wsclean/CMake/
 		cp -v deconvolution/*.{h,cpp} /tmp/wsclean/deconvolution/
 		cp -v idg/*.{h,cpp} /tmp/wsclean/idg/
